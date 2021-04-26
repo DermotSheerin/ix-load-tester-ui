dc5d982ad441c4aee813659c6d3e54939702b0be
Dermot Sheerin <sheerin@avaya.com>
2021-04-26 16:53:51 +0100

    refactor reducer to handle strings

--- src/components/hooks/useReducerConfig.js
+++ src/components/hooks/useReducerConfig.js
@@ -1,17 +1,27 @@
 // onChange event sets all values to strings therefore there are a number of conditionals below to parse st
 
 // convert string values to boolean for payload type radio
-const covertToBool = (payload) => {
-  return payload == 'true'
+// const covertToBool = (payload) => {
+//   return payload == 'true'
+// }
+
+const convertPayload = (payloadType, payload) => {
+  if (payloadType === 'number') {
+    return parseInt(payload);
+  } else if (payloadType === 'radio') {
+    return payload == 'true';
+  } else return payload;
 }
 
 export const reducerMainConfig = (state, action) => {
+    // if payload is a number then parse to an int
+    // if (action.payloadType === 'number') action.payload = parseInt(action.payload)
+    // if (action.payloadType === 'radio') action.payload = covertToBool(action.payload)
+    action.payload = convertPayload(action.payloadType, action.payload)
+    console.log(`inside reducer, payloadType is: ${typeof(action.payload)} and value is ${action.payload} }`)
+
     switch (action.type) {
       case "handle-input":
-        // if payload is a number then parse to an int
-        if (action.payloadType === 'number') action.payload = parseInt(action.payload)
-        if (action.payloadType === 'radio') action.payload = covertToBool(action.payload)
-        console.log(`inside reducer, payloadType is: ${typeof(action.payload)} and value is ${action.payload} }`)
         return {
           ...state,
           [action.field]: action.payload,
@@ -22,12 +32,15 @@ export const reducerMainConfig = (state, action) => {
   };
   
 export const reducerTenantList = (state, action) => {
+    // if payload is a number then parse to an int
+    // if (action.payloadType === 'number') action.payload = parseInt(action.payload)
+    action.payload = convertPayload(action.payloadType, action.payload);
+    console.log(`inside reducer, payloadType is: ${typeof(action.payload)} and value is ${action.payload} }`);
+
     switch (action.type) {
       case "tenantList-update":
         // create a new array to modify using spread operator
         const newState = [...state];
-        // if payload is a number then parse to an int
-        if (action.payloadType === 'number') action.payload = parseInt(action.payload)
         // access the index of the newState array that is being modified
         newState[action.index] = {
           // using the spread operator to extract the fields from the newState array (at index specified) and modify the required action field
